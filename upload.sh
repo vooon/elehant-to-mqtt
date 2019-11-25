63d0e5172d832f17f5066ece1d13b5617db7d17f
Vladimir Ermakov <vooon341@gmail.com>
2019-11-25 11:36:41 +0300

    update upload for pio 4.1

--- upload.sh
+++ upload.sh
@@ -6,7 +6,7 @@ upload() {
 	local env=$1
 	local file=$2
 
-	rsync $PWD/.pioenvs/$env/firmware.bin root@multivax.lan:/raid/srv/fd-ota/$env/$file
+	rsync $PWD/.pio/build/$env/firmware.bin root@multivax.lan:/raid/srv/fd-ota/$env/$file
 }
 
 upload elehant fw.bin
