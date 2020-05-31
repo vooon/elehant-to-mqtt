45d6b429a28f2f154b64e5fab78a2f7f43a0fc23
Vladimir Ermakov <vooon341@gmail.com>
2020-05-31 19:58:58 +0300

    esphome: tune ble tracker

--- esphome/elehant_ble.h
+++ esphome/elehant_ble.h
@@ -16,7 +16,7 @@ class ElehantBLE : public Component, public esp32_ble_tracker::ESPBTDeviceListen
 		bool parse_device(const esp32_ble_tracker::ESPBTDevice &device) override {
 
 
-			ESP_LOGD(TAG, "parse_device(): kuku! %s", device.address_str().c_str());
+			ESP_LOGI(TAG, "parse_device(): kuku! %s", device.address_str().c_str());
 
 			// this devices have MAC starting from: B0:01:02:
 			uint64_t mac = device.address_uint64();
@@ -36,8 +36,48 @@ class ElehantBLE : public Component, public esp32_ble_tracker::ESPBTDeviceListen
 
 			auto mfg_datas = device.get_manufacturer_datas();
 			if (mfg_datas.empty()) {
-				ESP_LOGD(TAG, "parse_device(): no mfg data");
+				ESP_LOGE(TAG, "parse_device(): no mfg data");
 			}
+			if (mfg_datas.size() > 1) {
+				ESP_LOGW(TAG, "mfg adv datas len: %d", mfg_datas.size());
+			}
+
+			auto &mfg_data = mfg_datas[0];
+
+			ESP_LOGI(TAG, "d uuid= %d", mfg_data.uuid.to_string().c_str());
+			ESP_LOGI(TAG, "d len= %d", mfg_data.data.size());
+
+			if (!mfg_data.uuid.contains(0xff, 0xff)) {
+				ESP_LOGE(TAG, "parse_device(): unexpected manufacturer data uuid: %s", mfg_data.uuid.to_string().c_str());
+				return false;
+			}
+			if (mfg_data.data.size() != 17) {
+				ESP_LOGE(TAG, "parse_device(): manufacturer data length %d != 17 bytes", mfg_data.data.size());
+				return false;
+			}
+			if (mfg_data.data[0] != 0x80) {
+				ESP_LOGE(TAG, "parse_device(): manufacturer data: wrong start byte");
+				return false;
+			}
+
+			uint8_t seq;
+			uint32_t device_num;
+			uint32_t counter;
+
+			seq = mfg_data.at(1);
+			memcpy(&device_num, &mfg_data.data.at(2), 3); device_num &= 0x00ffffff;
+			memcpy(&counter, &mfg_data.data.at(5), 4);
+
+			if (serial_no != device_num) {
+				ESP_LOGE(TAG, "parse_device(): serial number from MAC and data do not match, likely that format is changed. %#06 != %#06", serial_no, device_num);
+				return false;
+			}
+
+			ESP_LOGI(TAG, "parse_device(): seq = %u", seq);
+
+			total_m3_->publish_state(counter / 0.0001);
+			total_l_->publish_state(counter / 0.1);
+			rssi_->publish_state(device.get_rssi());
 
 			return false;
 		}
