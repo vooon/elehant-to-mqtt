efb078b61252092e1ce09cd152a9296393643e6c
Vladimir Ermakov <vooon341@gmail.com>
2023-12-01 12:33:47 +0100

    update OTA server

--- upload.sh
+++ upload.sh
@@ -6,7 +6,7 @@ upload() {
 	local env=$1
 	local file=$2
 
-	rsync $PWD/.pio/build/$env/firmware.bin root@multivax.lan:/raid/srv/fd-ota/$env/$file
+	rsync -P $PWD/.pio/build/$env/firmware.bin root@esp.vehq.ru:/data/fd-ota/$env/$file
 }
 
 upload elehant fw.bin
